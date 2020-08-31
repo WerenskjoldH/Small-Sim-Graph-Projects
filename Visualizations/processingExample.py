32bc7e5535d4796c73f3e6225b349b00cf04223e
Werenskjold <Rilliden@gmail.com>
2020-08-31 09:25:30 +0900

    Changed Processing Example

--- Visualizations/processingExample.py
+++ Visualizations/processingExample.py
@@ -2,19 +2,37 @@ from p5 import *
 
 
 def setup():
-    size(640, 360)
-    # no_stroke()
-    background(204)
+    size(360, 360)
+    fill(255, 255, 255)
 
 
 def draw():
-    fill(0, 255, 0)
+    background(0)
 
-    line((320, 180), (mouse_x, mouse_y))
+    stroke(255)
+    rotate_x(PI * 3/2 - 0.001)
+    rotate_z(PI/6 + 0.001 * millis())
+
+    fill(255)
+
+    begin_shape()
+    vertex(-100, -100, -100)
+    vertex(100, -100, -100)
+    vertex(0, 0, 100)
+
+    vertex(100, -100, -100)
+    vertex(100, 100, -100)
+    vertex(0, 0, 100)
+
+    vertex(100, 100, -100)
+    vertex(-100, 100, -100)
+    vertex(0, 0, 100)
+
+    vertex(-100, 100, -100)
+    vertex(-100, -100, -100)
+    vertex(0, 0, 100)
+
+    end_shape()
 
 
-def key_pressed(event):
-    background(204)
-
-
-run()
+run(mode="P3D")
